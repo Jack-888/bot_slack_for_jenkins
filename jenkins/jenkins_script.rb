2f9eb891fa783a21664b8b8fbc697bebabd4fc91
Jack-888 <vasya.shevchenko.2011@gmail.com>
2017-09-28 19:04:33 +0300

    add jenkins def

--- jenkins/jenkins_script.rb
+++ jenkins/jenkins_script.rb
@@ -2,42 +2,59 @@ require 'jenkins_api_client'
 require 'pry'
 
 module JenkinsApi
-  class Builder
+  class ProjetJenkins
     def initialize
       @client = JenkinsApi::Client.new(:server_ip => 'jenkins.andersenlab.com',
                                        :server_port => '80',
                                        :username => 'v.shevchenko',
                                        :password => '0ed19e3c034d74e37a167263f8802b70')
+
     end
 
-    def build_job_jenkins(way_project)
-      @client.job.build(way_project)
+    def jenkins_build_job (way_project)
+      if check_status(way_project) != "running" #|| "failure" || "unstable" || "not_run"
+        p @client.job.build(way_project)
+      else
+        p "Project is running"
+      end
     end
 
+    def jenkins_stop_build_job (way_project)
+      if check_status(way_project) == "running"
+        @client.job.stop_build(good_projct)
+      else
+        p "Project is not running"
+      end
+    end
+
+
+    def jenkins_status_project (way_project)
+      status_project = check_status(way_project)
+      if status_project == "running"
+        p 'Jean: %projectname% is building now.'
+        elsif status_project == "success"
+        p "Jean: %projectname% was built at #{time(way_project)} with status #{status_project}."
+        else
+      end
+    end
+
+    def monitoring
+
+    end
+
+    private
+
+    def check_status(way)
+      @client.job.get_current_build_status(way)
+    end
+
+    def time(way_project)
+      build_number = @client.job.get_current_build_number(way_project)
+      time = @client.job.get_build_details(way_project, build_number)["timestamp"]
+      Time.at(time/1000).strftime("%b %d, %Y %l:%M:%S %p")
+    end
+
+
   end
-
-  class Status
-
-  end
-
-  class Monitoring
-
-  end
-
 end
-
-# JenkinsApi::Builder.new.some_def(some)
-# JenkinsApi::Builder.some_def(some)
-
-
-name = "DevopsTest/job/GradleUnitTest"
-JenkinsApi::Builder.new.build_job_jenkins(name)
-
-
-# puts @client.job.list("DevopsTest")
-# puts @client.job.list_all
-# puts @client.job
-
-
-
 
